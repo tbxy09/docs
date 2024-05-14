c69b85a2dffd7de1f1825a446f540e091a521ed1
tbxy09 <tb_xy09@163.com>
2024-05-14 21:18:00 +0800

    add example

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,15 +1,20 @@
 import { ThemedImage } from "./ThemedImage";
+import Link from "next/link"; // Import Link from next/link
 
 export function Logo() {
   return (
-    <a href="/">
-      <ThemedImage
-        height={32}
-        width={170}
-        src="/img/logos/banner.svg"
-        darkImage={"/img/logos/banner.dark.svg"}
-        alt="BlockNote"
-      />
-    </a>
+    <Link href="/">
+      {" "}
+      {/* Use Link instead of <a> */}
+      <a>
+        <ThemedImage
+          height={32}
+          width={170}
+          src="/img/logos/banner.svg"
+          darkImage={"/img/logos/banner.dark.svg"}
+          alt="BlockNote"
+        />
+      </a>
+    </Link>
   );
 }
